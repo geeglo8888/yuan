f056f3fefb661c8f0271d060422faada783510c2
geeglo8888 <geeglo8888@hotmail.com>
2025-07-22 21:51:23 +0800

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,11 +1,11 @@
-// 必须使用默认导出，且接收request参数
 export default async (request) => {
-    // 返回JSON响应，指定正确的Content-Type
-    return new Response(JSON.stringify({ status: "ok", message: "Functions工作正常" }), {
+    return new Response(JSON.stringify({ 
+        status: "ok", 
+        message: "Functions工作正常" 
+    }), {
         headers: {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*" // 允许跨域访问
+            "Access-Control-Allow-Origin": "*"
         }
     });
 };
-    
